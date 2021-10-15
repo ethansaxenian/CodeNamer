b87ade3ad24ee359ab213974d0704fcde152ce5c
Ethan Saxenian <esaxenian@middlebury.edu>
2021-10-14 23:10:33 -0400

    add assets and simplify api

--- api/api.py
+++ api/api.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from api.word_association import load_model, get_similar_words, is_valid_clue
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-@cross_origin()
 def home_route():
     return "hello world"
 
 
 @app.route("/words/<word>")
 @app.route("/words/<word>/<num>")
-@cross_origin()
 def get_similar_words(word, num=100):
     model = load_model('wordVectors')
     clues = model.most_similar(word, topn=int(num))
