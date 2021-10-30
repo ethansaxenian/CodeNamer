a662a1e68af3bc54e7a6b383961c5b66c8ddb585
Ethan Saxenian <67524957+ethansaxenian@users.noreply.github.com>
2021-10-30 11:47:53 -0400

    Revert "Iosapp"

---
+++ README.md
@@ -0,0 +1,22 @@
+# CodeNamer
+A Codenames bot
+
+## TODO:
+- image processing to get words and colors from images
+- extend codenames functionality
+
+
+## Installation and Setup Instructions
+
+Install dependencies:
+```
+npm install
+```
+
+To run the development server:
+```
+npm start
+```
+
+
+The code for the python api is located at https://github.com/ethansaxenian/CodeNamer-api
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-/**
- * @format
- */
+import 'react-native-gesture-handler';
+import { registerRootComponent } from 'expo';
 
-import { registerRootComponent } from "expo";
-import {AppRegistry} from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
 
-// if developing in Xcode, uncomment this line:
-AppRegistry.registerComponent(appName, () => App);
-
-// if developing in Expo, uncomment this line:
-// registerRootComponent(App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
