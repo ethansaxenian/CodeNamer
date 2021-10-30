6dfc75e397e3947ebafb53a5befbe45688c47144
Ethan Saxenian <esaxenian@middlebury.edu>
2021-10-30 11:43:46 -0400

    set up app for expo

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
+import { registerRootComponent } from "expo";
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+// if developing in Xcode, uncomment this line:
 AppRegistry.registerComponent(appName, () => App);
+
+// if developing in Expo, uncomment this line:
+// registerRootComponent(App);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import _ from 'lodash';
-import Images from './ImagePicker';
 
 export default function App() {
   const [words, setWords] = useState();
-  const[colorImage, setColorImage] = useState(null);
-  const[gameImage, setGameImage] = useState(null);
-  const[colors, setColors] = useState(null);
-  const[game, setGame] = useState(null);
   const [userInput, setUserInput] = useState("");
 
   const getWords = async () => {
@@ -21,64 +16,15 @@ export default function App() {
     setWords(fetchedWords);
   }
 
-  useEffect(() => {
-    const getColorCode = async(imgEncoding) => {
-      const response =  await fetch(`http://127.0.0.1:5000/colors`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: imgEncoding
-      });
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      const fetchedColors = await response.json();
-      setColors(fetchedColors);
-      return fetchedColors
-    };
-
-    if(colorImage){
-       getColorCode(colorImage.base64);
-    }
-  }, [colorImage]);
-
-  useEffect(() => {
-    const getGameBoard = async(imgEncoding) => {
-      const response =  await fetch(`http://127.0.0.1:5000/gameboard`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: imgEncoding
-      });
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      const fetchedGame = await response.json();
-      setGame(fetchedGame);
-      console.log(fetchedGame);
-      return fetchedGame
-    };
-
-    if(gameImage){
-       getGameBoard(gameImage.base64);
-    }
-  }, [gameImage]);
-
   return (
     <View style={styles.container}>
-      {colorImage?  <Text>Color Card: </Text>: <Text>Upload your Color Card: </Text>}
-      {colorImage?( <Image source={{ uri: colorImage.uri }} style={{ width: 200, height: 200 }}/>): <Images setImage = {setColorImage}/>}
-      {colors?  <Text>{colors}</Text>: null}
-      {/* <TextInput style={styles.input} onChangeText={setUserInput} value={userInput} placeholder="enter a word"/>
+      <TextInput style={styles.input} onChangeText={setUserInput} value={userInput} placeholder="enter a word"/>
       <Button onPress={() => getWords()} disabled={!userInput} title="Get Words"/>
       {words ? (
         _.take(_.sortBy(_.toPairs(words), ([word, score]) => -score), 10).map(([word, score]) => <Text key={word}>{word}: {_.round(score, 3)}</Text>)
       ) : (
         <Text>loading...</Text>
-      )} */}
-      {gameImage? <Text>Game Board: </Text>:  <Text>Upload your Game Board: </Text>}
-      {gameImage?  <Image source={{ uri: gameImage.uri }} style={{ width: 300, height: 200 }}/>: <Images setImage = {setGameImage}/>}
-      {game?  <Text>{game}</Text>: null}
-
-
+      )}
     </View>
   );
 }
